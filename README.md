7720bab9366ce672e85596bd71686b2e47e6646b
Rasul Agasiyev <rslgszd@gmail.com>
2022-09-29 20:24:16 +0400

    My commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# javascript-15-project
---
+++ counter/app.js
@@ -0,0 +1,27 @@
+// initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    } else if (count > 0) {
+      value.style.color = "green";
+    } else {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
